a90b1fa00831286a78c593716898ea7f53503be1
Pranav AVN <pranavavn@outlook.com>
2025-02-13 18:59:19 +0530

    Rudimentary sbrk() impl

--- palloc.c
+++ palloc.c
@@ -1,4 +1,26 @@
+#include <unistd.h>
 #include <stdio.h> //debug printf
+
+typedef char ALIGN[16];
+
+union header{
+    struct header_t{
+        size_t size;
+        unsigned is_free;
+        struct header_t *next;
+    } s;
+    ALIGN stub;
+};
+
+typedef union header header_t;
+
+void *malloc(size_t size){
+    void *block;
+    block = sbrk(size);
+    if(block == (void*) -1) return NULL;
+    return block;
+}
+
 
 int main(){
     printf("Hello World\n");
